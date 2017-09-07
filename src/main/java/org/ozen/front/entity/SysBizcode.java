fad4b17437d1b85ebb9a5957bbf7005dedba8402
13307193267 <136642169@qq.com>
2017-09-07 22:02:40 +0800

    更新后的实体类

---
+++ src/main/java/org/ozen/front/entity/SysBizcode.java
@@ -0,0 +1,73 @@
+package org.ozen.front.entity;
+
+public class SysBizcode {
+    private Long id;
+
+    private String text;
+
+    private String code;
+
+    private String type;
+
+    private String typeName;
+
+    private Boolean status;
+
+    private String remark;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text == null ? null : text.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName == null ? null : typeName.trim();
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
---
+++ src/main/java/org/ozen/front/entity/TbUserAdvise.java
@@ -0,0 +1,45 @@
+package org.ozen.front.entity;
+
+import java.util.Date;
+
+public class TbUserAdvise {
+    private Integer id;
+
+    private Long userId;
+
+    private Date createTime;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
