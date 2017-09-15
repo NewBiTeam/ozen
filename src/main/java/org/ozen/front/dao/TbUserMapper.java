f384045473d004d48fc4a00f7c126239611e7537
13307193267 <136642169@qq.com>
2017-09-15 10:31:36 +0800

    Login 登录粗略完成

--- src/main/java/org/ozen/front/dao/TbUserMapper.java
+++ src/main/java/org/ozen/front/dao/TbUserMapper.java
@@ -2,7 +2,7 @@ package org.ozen.front.dao;
 
 import org.ozen.front.entity.TbUser;
 
-public interface TbUserDao {
+public interface TbUserMapper {
 
 	TbUser login(TbUser user);
 }
--- src/main/java/org/ozen/front/entity/TbUser.java
+++ src/main/java/org/ozen/front/entity/TbUser.java
@@ -34,6 +34,10 @@ public class TbUser implements Serializable{
 
 	    private Date loginTime;
 
+	    private String username;
+	    
+	    private String password;
+	    
 	    public Long getUserId() {
 	        return userId;
 	    }
@@ -153,4 +157,22 @@ public class TbUser implements Serializable{
 	    public void setLoginTime(Date loginTime) {
 	        this.loginTime = loginTime;
 	    }
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	    
+	    
 }
