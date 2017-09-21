2208a567d29fda45e0d1c646d661a1dc9bbd7d1a
13307193267 <136642169@qq.com>
2017-09-21 14:05:59 +0800

    about.jsp+log日志更新

--- src/main/webapp/resources/js/external.js
+++ src/main/webapp/resources/js/external.js
@@ -5,13 +5,30 @@ $(function(){
 		$("#shouru").show();
 		$("#zhichu").show(); 
 	});
+	
 	$("#shouru").click(function(){
-		$("#sy").html("<form class='form-horizontal'>" +
+		$("#zhichus").remove();
+		$("#erbu").remove();
+		$("#sy").html("<form class='form-horizontal' id='shourus'>" +
 				"<fieldset><legend>收入</legend>" +
 				"<div class='form-group'><label class='col-md-4 control-label' for='textinput'>入账时间：</label>  <div class='col-md-4'><input id='time' name='textinput' type='date' class='form-control input-md'> </div></div>" +
 				"<div class='form-group'><label class='col-md-4 control-label' for='textinput'>入账金额：</label>  <div class='col-md-4'><input id='money' name='textinput' type='text' placeholder='请输入入账金额' class='form-control input-md'> </div></div>" +
-				"<div class='form-group'><label class='col-md-4 control-label' for='textinput'>Text Input</label>  <div class='col-md-4'><input id='textinput' name='textinput' type='text' placeholder='placeholder' class='form-control input-md'> </div></div>" +
-				"<div class='form-group'><label class='col-md-4 control-label' for='textarea'>备注</label><div class='col-md-4'><textarea class='form-control' id='textarea' name='textarea'>default text</textarea> </div></div>"+
+				"<div class='form-group'><label class='col-md-4 control-label' for='radios'>是否公开：</label><div class='col-md-4'> <label class='radio-inline' for='radios-0'><input type='radio' name='radios' id='radios-0' value='1' checked='checked'>他人可见 </label> <label class='radio-inline' for='radios-1'><input type='radio' name='radios' id='radios-1' value='2'>他人不可见</label> </div></div>"+
+				"<div class='form-group'><label class='col-md-4 control-label' for='textarea'>备注：</label><div class='col-md-4'><textarea class='form-control' id='describes' name='textarea'></textarea> </div></div>"+
+				"<div class='form-group'><label class='col-md-4 control-label' for='button1id'></label><div class='col-md-8'><button id='button1id' name='button1id' class='btn btn-success'>提交</button>&emsp;<button id='button2id' name='button2id' class='btn btn-danger'>取消</button></div></div>"+
+				"</fieldset></form>")
+		
+	});
+	$("#zhichu").click(function(){
+		$("#shourus").remove();
+		$("#erbu").remove();
+		$("#sy").html("<form class='form-horizontal' id='zhichus'>" +
+				"<fieldset><legend>支出</legend>" +
+				"<div class='form-group'><label class='col-md-4 control-label' for='textinput'>出账时间：</label>  <div class='col-md-4'><input id='time' name='textinput' type='date' class='form-control input-md'> </div></div>" +
+				"<div class='form-group'><label class='col-md-4 control-label' for='textinput'>出账金额：</label>  <div class='col-md-4'><input id='money' name='textinput' type='text' placeholder='请输入入账金额' class='form-control input-md'> </div></div>" +
+				"<div class='form-group'><label class='col-md-4 control-label' for='radios'>是否公开：</label><div class='col-md-4'> <label class='radio-inline' for='radios-0'><input type='radio' name='radios' id='radios-0' value='1' checked='checked'>他人可见 </label> <label class='radio-inline' for='radios-1'><input type='radio' name='radios' id='radios-1' value='2'>他人不可见</label> </div></div>"+
+				"<div class='form-group'><label class='col-md-4 control-label' for='textarea'>备注：</label><div class='col-md-4'><textarea class='form-control' id='describes' name='textarea'></textarea> </div></div>"+
+				"<div class='form-group'><label class='col-md-4 control-label' for='button1id'></label><div class='col-md-8'><button id='button1id' name='button1id' class='btn btn-success'>提交</button>&emsp;<button id='button2id' name='button2id' class='btn btn-danger'>取消</button></div></div>"+
 				"</fieldset></form>")
 		
 	});
